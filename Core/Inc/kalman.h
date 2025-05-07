e0bb338ef2f5271f69da622d308b8a4bee78207b
Arthur1743 <quangdungho30@gmail.com>
2025-05-07 22:04:33 +0700

    change

--- Core/Inc/kalman.h
+++ Core/Inc/kalman.h
@@ -1,30 +1,79 @@
-/*
- * kalman.h
- *
- *  Created on: May 7, 2025
- *      Author: Admin
+/**
+ * @file kalman.h
+ * @brief Header file for standalone Kalman filter implementation for sensor fusion
+ * @author Claude
+ * @date May 7, 2025
  */
 
-// kalman_filter.h
-#ifndef KALMAN_H
-#define KALMAN_H
-
-typedef struct {
-    float x[4];      // State: [phi, phi_dot, theta, theta_dot]
-    float P[4][4];   // Error covariance matrix
-    float Q[4][4];   // Process noise covariance
-    float R[2][2];   // Measurement noise covariance
-    float K[4][2];   // Kalman gain
-    float gyro_bias[3]; // Gyroscope bias estimates [p_bias, q_bias, r_bias]
-} KalmanEKFFilter;
-
-void KalmanEKF_Init(KalmanEKFFilter *kf);
-void KalmanEKF_Update(KalmanEKFFilter *kf, float phi_acc, float theta_acc,
-                      float p, float q, float r, float dt, float acc_magnitude);
-void KalmanEKF_GetAttitude(KalmanEKFFilter *kf, float *roll, float *pitch,
-                           float *roll_rate, float *pitch_rate);
-void KalmanEKF_GetGyroBias(KalmanEKFFilter *kf, float *p_bias, float *q_bias, float *r_bias);
-void KalmanEKF_Reset(KalmanEKFFilter *kf, float init_roll, float init_pitch);
-
-#endif
-
+ #ifndef KALMAN_FUSION_H
+ #define KALMAN_FUSION_H
+ 
+ #include <stdint.h>
+ 
+ /* Conversion constant */
+ #define RAD_TO_DEG 57.295779513082320876798154814105f
+ 
+ /**
+  * @brief Kalman filter structure for sensor fusion
+  */
+ typedef struct {
+     float Q_angle;    /* Process noise variance for angle */
+     float Q_bias;     /* Process noise variance for bias */
+     float R_measure;  /* Measurement noise variance */
+     
+     float angle;      /* Estimated angle */
+     float bias;       /* Estimated gyro bias */
+     
+     float P[2][2];    /* Error covariance matrix */
+ } Kalman_t;
+ 
+ /**
+  * @brief Accelerometer data structure
+  */
+ typedef struct {
+     float x;  /* X-axis acceleration in g */
+     float y;  /* Y-axis acceleration in g */
+     float z;  /* Z-axis acceleration in g */
+ } AccelData_t;
+ 
+ /**
+  * @brief Gyroscope data structure
+  */
+ typedef struct {
+     float x;  /* X-axis angular rate in degrees/second */
+     float y;  /* Y-axis angular rate in degrees/second */
+     float z;  /* Z-axis angular rate in degrees/second */
+ } GyroData_t;
+ 
+ /**
+  * @brief Combined IMU data structure
+  */
+ typedef struct {
+     AccelData_t accel;  /* Accelerometer data */
+     GyroData_t gyro;    /* Gyroscope data */
+ } IMUData_t;
+ 
+ /**
+  * @brief Angle output structure
+  */
+ typedef struct {
+     float roll;        /* Roll angle (filtered) in degrees */
+     float pitch;       /* Pitch angle (filtered) in degrees */
+     float roll_accel;  /* Roll angle from accelerometer only in degrees */
+     float pitch_accel; /* Pitch angle from accelerometer only in degrees */
+ } Angles_t;
+ 
+ /* Function prototypes */
+ void Kalman_Init(Kalman_t *kalman);
+ void Kalman_SetParameters(Kalman_t *kalman, float Q_angle, float Q_bias, float R_measure);
+ void Kalman_SetAngle(Kalman_t *kalman, float initialAngle);
+ float Kalman_Update(Kalman_t *kalman, float newAngle, float newRate, float dt);
+ void Kalman_CalculateAccelAngles(const AccelData_t *accel_data, Angles_t *angles);
+ void Kalman_UpdateAttitude(const AccelData_t *accel_data, const GyroData_t *gyro_data,
+                           Kalman_t *roll_kalman, Kalman_t *pitch_kalman,
+                           Angles_t *angles, float dt);
+ void Kalman_ProcessSensorFusion(const IMUData_t *imu_data, 
+                                Kalman_t *roll_kalman, Kalman_t *pitch_kalman,
+                                Angles_t *angles, float dt);
+ 
+ #endif /* KALMAN_FUSION_H */
